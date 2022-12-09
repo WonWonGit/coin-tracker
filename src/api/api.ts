1268af167e1a334c16b53dc640278f4f0193127b
WonWonGit <bkn020612@gmail.com>
2022-12-09 23:53:37 +0900

    react-query api

---
+++ src/api/api.ts
@@ -0,0 +1,22 @@
+const BASE_URL = "https://api.coinpaprika.com/v1";
+
+export function fetchConis(){
+    return fetch(`${BASE_URL}/coins`)
+                .then((response) => response.json());
+}
+
+export function fetchCoinInfo(coinId?:String){
+    return fetch(`${BASE_URL}/coins/${coinId}`)
+                .then((response) => response.json());
+}
+
+export function fetchCoinTickers(coinId?:String){
+    return fetch(`${BASE_URL}/tickers/${coinId}`)
+                .then((response) => response.json());
+}
+
+export function fetchCoinHistory(coinId: String){
+
+    return fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)
+                .then((response) => response.json());
+}
